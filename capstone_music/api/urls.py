003b4d69a495cb7e564f549495e76c445e6d38bc
Melina Nadal <melinanhopkins@gmail.com>
2021-01-19 16:09:47 -0500

    adds migrations

--- capstone_music/api/urls.py
+++ capstone_music/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom, UpdateRoom
 
 urlpatterns = [
-    path('room', RoomView.as_view()),
+    path('Room', RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
     path('get-room', GetRoom.as_view()),
     path('join-room', JoinRoom.as_view()),
