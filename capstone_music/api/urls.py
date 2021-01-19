1d9ff132f3ad0df9616790698c39b24eb2a522eb
Melina Nadal <melinanhopkins@gmail.com>
2021-01-19 12:33:10 -0500

    debug

--- capstone_music/api/urls.py
+++ capstone_music/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom
 
 urlpatterns = [
     path('Room', RoomView.as_view()), path('create-room', CreateRoomView.as_view()),
-     path('get-room', GetRoom.as_view())
-]
+     path('get-room', GetRoom.as_view()),
+     path('join-room', JoinRoom.as_view())
 ]
