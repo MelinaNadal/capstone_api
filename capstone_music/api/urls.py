69ca1e7f29193c4048e19e1cba1ebf3179a94ac3
Melina Nadal <melinanhopkins@gmail.com>
2021-01-18 20:29:21 -0500

    completes set up

--- capstone_music/api/urls.py
+++ capstone_music/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import RoomView
 
 urlpatterns = [
-    path('room', RoomView.as_view()),
+    path('Room', RoomView.as_view()),
 ]
---
+++ capstone_music/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class RoomJoinPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <p>Join a party lounge to guest DJ</p>;
+  }
+}
